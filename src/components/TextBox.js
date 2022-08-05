256981643c6cd289ffb46475f4a643776aabab1a
Sai <sai@SAIs-MacBook-Pro.local>
2022-08-06 00:23:14 +0530

    added add and delete funtionality

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,13 +1,17 @@
 import "../styles/Admin.css";
 import close from "../asset/close.png";
 
-const TextBox = ({ ingredient }) => {
+const TextBox = ({ data, index, deleteItem }) => {
+	console.log("index", index);
 	return (
-		<div className="textbox">
+		<div className="textbox" style={{ marginRight: "10px" }}>
 			<span style={{ marginTop: "5px", marginLeft: "10px" }}>
-				<p>{ingredient}</p>
+				<p>{data}</p>
 			</span>
-			<span style={{ marginRight: "2px" }}>
+			<span
+				style={{ marginRight: "2px", cursor: "pointer" }}
+				onClick={() => deleteItem(index)}
+			>
 				<img src={close} alt="close" />
 			</span>
 		</div>
