d89118dfd7835bba345215997fa0e58da8bc4487
Sai <sai@SAIs-MacBook-Pro.local>
2022-08-10 22:57:02 +0530

    added menu route to order now button

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
 import "../styles/Info.css";
 const Info = ({ title, description, buttonText }) => {
 	return (
 		<div className="info-container">
 			<div style={{ fontWeight: "bold", fontSize: "24px" }}>{title}</div>
 			<div>{description}</div>
-			<div className="order-now-button">
-				<p style={{ textAlign: "center", fontWeight: "bold" }}>{buttonText}</p>
-			</div>
+			<Link
+				to={`/menu`}
+				style={{ color: "inherit", textDecoration: "inherit" }}
+			>
+				<div className="order-now-button">
+					<p style={{ textAlign: "center", fontWeight: "bold" }}>
+						{buttonText}
+					</p>
+				</div>
+			</Link>
 		</div>
 	);
 };
